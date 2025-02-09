bec85887d5fb85ad8406be1f03b6c2079e364fe5
Varantt <varant@itxi.net>
2025-02-09 15:58:07 +0200

    (feat) -- created initial flightSearch component

---
+++ src/components/FlightSearch/FlightSearch.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './FlightSearch.scss';
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+
+export const FlightSearch : React.FC = () => {
+  return (
+    <div id = "flight-search">
+
+
+        <div className="flight-search-header">
+            <div className="round-trip">
+                Round Trip
+            </div>
+
+            <div className="number-of-people">
+
+            </div>
+
+
+            <div className="type">
+
+            </div>
+        </div>
+    </div>
+  )
+}
